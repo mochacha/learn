2c587d049f35b5877eb7b6ab618cb93de73b843a
mochacha <tstudio@126.com>
2021-03-25 23:11:21 +0800

    outdate

---
+++ csharp-learning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
