c4b3f38ad4bed05a79324c225b2aba09547a7983
mochacha <tstudio@126.com>
2021-04-03 16:31:23 +0800

    2021-04-03-csharp作业

---
+++ csharp-learning-2021-04-04-作业/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace csharp_learning_2021_04_04_作业
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i == 1)
+            {
+
+
+                Console.WriteLine("输入验证码(123bbb)");
+                var keyword = Console.ReadLine();
+
+                if (keyword == "123bbb")
+                {
+                    i = 2;
+                    while (i == 2)
+                    {
+                        Console.WriteLine("请输入用户名(yefei)");
+                        var user = Console.ReadLine();
+
+                        if (user == "yefei")
+                        {
+                            i = 3;
+                            while (i == 3)
+                            {
+                                Console.WriteLine("请输入密码(123)");
+                                var password = Console.ReadLine();
+
+
+
+                                if (password == "123")
+                                {
+                                    i = 4;
+                                    Console.WriteLine("登录成功");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("用户名或密码错误");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("用户名不存在");
+                        }
+                    }
+                }
+                else
+                {
+                    i = 1;
+                    Console.WriteLine("验证码错误");
+                }
+            }
+        }
+    }
+}
