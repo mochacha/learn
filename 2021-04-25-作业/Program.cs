337fc13187898a0b82efb3fc95c19d602a406b73
mochacha <tstudio@126.com>
2021-04-26 16:52:30 +0800

    作业更新

--- 2021-04-25-作业/Program.cs
+++ 2021-04-25-作业/Program.cs
@@ -10,12 +10,28 @@ namespace _2021_04_25_作业
             Console.WriteLine("Hello World!");
             for(int i=0;i<10;i++)
             {
-                Console.WriteLine("请输入不超过1000的自然数");
-               double input= Console.Read();
-                bool y = GuessMe(input, number());
-                if (y) { Console.WriteLine("太大了"); }
                 
-                else { Console.WriteLine("太小了"); }
+                Console.WriteLine("请输入不超过1000的自然数");
+                var r =Console.ReadLine();
+                int y = GuessMe(Convert.ToInt32( r), Math.Round(number()));
+                if (y == 1) { Console.WriteLine($"太大了,（还剩{10-i}次）"); }
+                if (y == 2) { Console.WriteLine($"太小了，（还剩{10-i}次）"); }
+                if (y == 3) 
+                {
+                    if (i < 5) 
+                    { 
+                        Console.WriteLine("牛逼");
+                        return;
+                    }
+                        
+                    else if(i<8)
+                    {
+                        Console.WriteLine("不错嘛");
+                        return;
+                    }
+                    else { Console.WriteLine("猜对了"); }
+
+                     }
                 
             }
         }
@@ -24,10 +40,11 @@ namespace _2021_04_25_作业
             double r = new Random().NextDouble();
             return 1000*r;            
         }
-        static bool GuessMe(double r,double i)
+        static int GuessMe(double r, double i)
         {
-            if (r >=i) { return true; }
-            else{ return false; }
+            if (r > i) { return 1; }
+            else  if (r < i) { return 2; }
+            else{ return 3; }
 
         }
     }
